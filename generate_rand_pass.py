11197af1f92f4c65d6ffc17159e72a5f37779d6a
Nikolay Shcheglov <ndnd@mail.ru>
2017-10-23 21:08:22 +0300

    Now randomization works with nanosecond last bit

--- generate_rand_pass.py
+++ generate_rand_pass.py
@@ -4,12 +4,19 @@ import sys
 import time
 
 
-def generate_random_byte_from_nanoseconds():
+def generate_random_bit_from_nanoseconds():
     random.seed()
+    time.sleep(random.randint(0, 127)*1e-4)
     cur_time_str = str('%.9f' % time.time())
-    nanoseconds = int(cur_time_str.split('.')[1])
-    byte = nanoseconds.to_bytes(4, byteorder='big')[3]
-    time.sleep(random.randint(0, 255)*1e-9)
+    nanoseconds = int(cur_time_str[-1])
+    bit = 1 if nanoseconds & 1 else 0
+    return bit
+
+
+def generate_random_byte():
+    byte = 0
+    for i in range(0, 7):
+        byte += pow(2, i)*generate_random_bit_from_nanoseconds()
     return byte
 
 
@@ -17,7 +24,7 @@ def generate_rand_pass(length):
     password = ''
     count = 0
     while count != length:
-        byte = chr(generate_random_byte_from_nanoseconds())
+        byte = chr(generate_random_byte())
         if byte in string.ascii_letters or byte in string.digits:
             password += byte
             count += 1
@@ -25,11 +32,10 @@ def generate_rand_pass(length):
 
 
 def main():
-    if len(sys.argv) == 1:
-        generate_rand_pass(sys.argv[1])
+    if len(sys.argv) == 2:
+        generate_rand_pass(int(sys.argv[1]))
     else:
         sys.exit('You need to specify the password size as first argument')
-
 
 if __name__ == "__main__":
     main()
